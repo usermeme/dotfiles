504462c1337302741fbeae04e9b0a47923861d84
Raman Yankovich <romanyankovich98@gmail.com>
2026-04-07 17:29:19 +0200

    chore: update blink lua

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,11 +1,25 @@
 return {
   {
     "saghen/blink.cmp",
+    version = "*",
+    opts_extend = { "sources.default" },
     opts = {
       completion = {
         accept = {
           auto_brackets = {
             enabled = false,
+          },
+        },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+
+        providers = {
+          lsp = {
+            score_offset = 100,
+          },
+          path = {
+            score_offset = -100,
           },
         },
       },
