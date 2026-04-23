3e0a4b000752ddf1474fb68fba6d107105b7d548
Raman Yankovich <romanyankovich98@gmail.com>
2026-04-23 10:32:15 +0200

    chore: adjust dot files

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -4,3 +4,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.cmd("silent! LspEslintFixAll")
   end,
 })
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = "*",
+})
