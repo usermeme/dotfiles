bd9ac36dacee666d554ac1cd73774d24ff77aa16
Raman Yankovich <romanyankovich98@gmail.com>
2026-05-21 22:46:43 +0200

    chore: update blink

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -23,27 +23,6 @@ return {
           },
         },
       },
-      keymap = {
-        preset = "super-tab",
-        ["<Tab>"] = {
-          function(cmp)
-            if vim.b[vim.api.nvim_get_current_buf()].nes_state then
-              cmp.hide()
-              return (
-                require("copilot-lsp.nes").apply_pending_nes()
-                and require("copilot-lsp.nes").walk_cursor_end_edit()
-              )
-            end
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          "snippet_forward",
-          "fallback",
-        },
-      },
     },
   },
 }
